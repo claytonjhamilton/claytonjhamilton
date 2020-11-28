6a9674093854e8d0c473be92d7e11461e60aadff
claytonjhamilton <claytonjhamilton@yahoo.com>
2020-11-27 23:28:48 -0700

    Add: Weather info & sentence logic

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 <h1 align = "center"> Hello there, my name is Clayton Hamilton, PharmD  <img src="https://github.com/claytonjhamilton/claytonjhamilton/blob/main/images/waving_hand.gif" width="40px"></h1>
 
-Pleasure to meet you. I'm a pharmacist informatician :pill: living in **Salt Lake City, Utah**. 
-
-Feel free to reach out to me on [![LinkedIn][1.1]][1].
+Pleasure to meet you. I'm a pharmacist informatician :pill: living in beautiful <b>Salt Lake City</b>. 
+<p>Today, the sun rose at <b>07:28 AM</b> and sets at <b>05:02 PM</b>. The tempature is currently <b>26.33°F</b> and the sky is clear which means I&#39;ll either be running or rock climbing.</p>
 
 <h3>My post(s)</h3>
 <ul>
@@ -17,9 +16,11 @@ Feel free to reach out to me on [![LinkedIn][1.1]][1].
 |:----|
 |<code><img height="20" src="https://img.shields.io/badge/-Microsoft%20SQL%20Server-grey?logo=microsoft-sql-server&logoColor=red"></code><code><img height="20" src="https://img.shields.io/badge/-Visual%20Studio%20Code-blue?logo=visual-studio-code"></code><code><img height="20" src="https://img.shields.io/badge/-Visual%20Studio-purple?logo=visual-studio"></code><code><img height="20" src="https://img.shields.io/badge/-Report%20Builder-white?logo=Power%20BI&logoColor=red"></code><code><img height="20" src="https://img.shields.io/badge/-PowerBI-black?logo=Power%20BI&logoColor=yellow"></code><code><img height="20" src="https://img.shields.io/badge/-Git-9cf?logo=git"></code><code><img height="20" src="https://img.shields.io/badge/-GitHub-black?logo=GitHub"></code>|
 
+You can find me on [![LinkedIn][1.1]][1].
+
 <hr style="width:75%;text-align:center">
 <p align="center">
-    <text>This README last auto-updated on: Thursday, November 26, 3:44 PM MST</text>
+    <text>This README last auto-updated on: Friday, November 27, 11:26 PM MST</text>
     <br>
 </p>
 <p align="center">
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
 const Mustache = require('mustache');
+const fetch = require("node-fetch");
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
@@ -15,11 +16,47 @@ let DATA = {
     timeZone: 'America/Denver',
   }),
 };
-/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
+//    `http://api.openweathermap.org/data/2.5/weather?zip=84105,us&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=standard`
+
+async function setWeatherInformation() {
+  await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?zip=84105,us&appid=1c249ad521ea3ac8f2ae3ec07dc9de6c&units=standard`
+    )
+    .then(r => r.json())
+    .then(r => {
+      // Convert Kelvin to Fahrenheit
+      DATA.city_temperature = (((Math.round(r.main.temp)-273.15)*1.8)+32).toFixed(2);
+      // Weather label      
+      if (r.weather[0].main == `Clear`) {
+        DATA.weather_description = `the sky is clear which means I'll either be running or rock climbing`;
+      } else if (r.weather[0].main == `Thunderstorm`)  {
+        DATA.weather_description = `it's currently storming so I may stay indoors and have a tea`;
+      } else if (r.weather[0].main == `Drizzle`) {
+        DATA.weather_description = `it's lightly raining so I may go for a run`;
+      } else if (r.weather[0].main == `Rain`) {
+        DATA.weather_description = `it's currently raining so I may stay indoors and have a tea`;
+      } else if (r.weather[0].main == `Snow`) {
+        DATA.weather_description = `snowflakes are falling and I'm considering snowboarding this weekend`;
+      } else if (r.weather[0].main == `Clouds`) {
+        DATA.weather_description = `it's a cloudy day and I'll likely go for a run`;
+      } else {
+        DATA.weather_description = `the atmosphere is hazy outside`;
+      }
+      DATA.city_name = r.name;
+      DATA.city_weather_icon = r.weather[0].icon;
+      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'America/Denver',
+      });
+      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'America/Denver',
+      });
+    });
+}
+
 function generateReadMe() {
   fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
     if (err) throw err;
@@ -27,4 +64,13 @@ function generateReadMe() {
     fs.writeFileSync('README.md', output);
   });
 }
-generateReadMe();
+
+async function action() {
+  // Fetch Weather
+  await setWeatherInformation();
+
+  // Generate README
+  await generateReadMe();
+};
+
+action();
